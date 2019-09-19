ac2956494d44148c9130384f493c59a2a65f3d34
Freddy Ernst <freddyernst@Freddys-MacBook-Pro.local>
2019-09-19 09:56:16 -0500

    Second commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const {SERVER_PORT} = process.env
 
 
+// auth endpoints
+app.get('/auth/user')
+app.post('/auth/register')
+app.post('/auth/login')
+app.post('/auth/logout')
+
+// forum endpoints
+
+
+
+
+// posts endpoints
+app.get('/api/forums')
+app.get('/api/posts/:topic')
+app.post('/api/posts/:topic')
+app.delete('/api/posts/:id')
 
 
 
